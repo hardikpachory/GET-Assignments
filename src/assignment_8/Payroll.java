c2ca40c089d48919989c7ab8c75236dfada262c7
Hardik Pachory <hardik.pachory@metacube.com>
2024-07-03 17:01:19 +0530

    Assignment 8 README

--- src/assignment_8/Payroll.java
+++ src/assignment_8/Payroll.java
@@ -2,7 +2,7 @@ package assignment_8;
 
 public class Payroll {
 	public static void printSalarySLip(Organization org) {
-		System.out.println("     Salary Slip of All Employees    ");
+		System.out.println("     Salary Slip of All Employees of " + org.getNameOfOrganization());
 		System.out.println("-------------------------------------\n");
 		for(Employee emp : org.getAllEmployees()) {
 			System.out.println("Employee ID  : " + emp.getId());
